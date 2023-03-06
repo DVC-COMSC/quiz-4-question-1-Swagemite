33cf6ef0d72702385dcd81450069a97841d61796
Brent Young <bryoung777@icloud.com>
2023-03-05 16:08:38 -0800

    Attempt 1

--- q1.py
+++ q1.py
@@ -1,6 +1,19 @@
 
 # ******************************
 # Make your Code
+flag = 0 
+count = 0
+for i in range(10):
+  num = int(input('enter a number: '))
+  if num % 2 == 0:
+    flag = flag + 1
+  else:
+    if flag >= 2:
+      count = count + 1
+      flag = 0
+if flag >= 2:
+  count = count + 1
+print(count)
 # ******************************
 
 
